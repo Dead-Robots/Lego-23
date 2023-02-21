36b4c0932cdffd3be148d2217aa0a659b6c56ccd
botball <mentors@deadrobots.com>
2023-02-20 21:02:18 -0500

    Line follows to botgal, grabs her, and begins delivery. Work on delivery.

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 #!/usr/local/bin/python3.10 -u
 
-from kipr import motor_power, msleep, enable_servos, set_servo_position, analog, push_button, freeze
+from kipr import motor_power, msleep, enable_servos, set_servo_position, analog, push_button, freeze, clear_motor_position_counter, get_motor_position_counter
 RIGHT_MOTOR = 0
 LEFT_MOTOR = 3
 ARM_SERVO = 1
 CLAW_SERVO = 0
 TOP_HAT = 0
-CLAW_OPEN = 250
-CLAW_CLOSE = 900
+CLAW_OPEN = 350
+CLAW_CLOSE = 1000
 
 
 def drive(left_speed, right_speed, time):
@@ -21,10 +21,10 @@ def line_follow(time):
     while x < time:
         if analog(TOP_HAT) < 1800:  # on white
             x += 10
-            drive(70, 100, 10)
+            drive(80, 100, 10)
         else:                       # on black
             x += 10
-            drive(100, 65, 10)
+            drive(100, 85, 10)
 
 
 def stop_motors():
@@ -42,23 +42,22 @@ def wait_for_button():
 
 
 def get_botgal():
-    drive(93, 100, 1150)                # move tophat out of start box
-    drive(40, 100, 500)                 # line up for line follow - needs work
-    drive(100, 40, 300)
-    wait_for_button()
-    line_follow(2000)                   # go to botgal
-    drive(93, 100, 800)                 # square up with pvc
+    drive(70, 100, 1000)                 # move tophat out of start box while lining up for line follow
+    drive(100, 80, 1000)
+    line_follow(1600)                    # go to botgal
+    drive(93, 100, 500)                  # square up with pvc
+    stop_motors()
     set_servo_position(CLAW_SERVO, CLAW_CLOSE)
 
 
 def deliver_botgal():
     drive(-100, -100, 800)
     drive(0, 100, 1050)                 # cross bump perpendicularly
-    drive(93, 100, 900)
+    drive(100, 100, 900)
     wait_for_button()
     drive(0, 100, 1050)
     wait_for_button()
-    drive(93, 100, 3000)
+    drive(100, 100, 3000)
 
 
 def start():
